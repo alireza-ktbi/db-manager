6ac3e42921ed7f932c0aa76fefbf0c2881b01220
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-07 11:46:14 +0330

    rename  to

--- src/DBMNGR.php
+++ src/DBMNGR.php
@@ -13,49 +13,49 @@ class DBMNGR
     /**
      * @throws DatabaseTypeNotFoundException
      */
-    public function createInstance(DBInfo $databaseInfo)
+    public function createInstance(DBInfo $DBInfo)
     {
-        self::$databases[$databaseInfo->DbName . $databaseInfo->DbType] = match ($databaseInfo->DbType) {
-            DBType::PDO => $this->createInstancePdo($databaseInfo),
-            DBType::MYSQL => $this->createInstanceMysqli($databaseInfo),
-            DBType::MONGODB => $this->creatInstanceMongoDb($databaseInfo),
+        self::$databases[$DBInfo->DbName . $DBInfo->DbType] = match ($DBInfo->DbType) {
+            DBType::PDO => $this->createInstancePdo($DBInfo),
+            DBType::MYSQL => $this->createInstanceMysqli($DBInfo),
+            DBType::MONGODB => $this->creatInstanceMongoDb($DBInfo),
             default => throw new DatabaseTypeNotFoundException()
         };
     }
 
-    private function createInstancePdo(DBInfo $databaseInfo): PDO
+    private function createInstancePdo(DBInfo $DBInfo): PDO
     {
-        $dsn = "mysql:host=$databaseInfo->DbHost;dbname=$databaseInfo->DbName";
-        if (isset($databaseInfo->DbPort)) $dsn .= ";port=" . $databaseInfo->DbPort;
-        return new PDO($dsn, $databaseInfo->DbUser, $databaseInfo->DbPass);
+        $dsn = "mysql:host=$DBInfo->DbHost;dbname=$DBInfo->DbName";
+        if (isset($DBInfo->DbPort)) $dsn .= ";port=" . $DBInfo->DbPort;
+        return new PDO($dsn, $DBInfo->DbUser, $DBInfo->DbPass);
     }
 
-    private function createInstanceMysqli(DBInfo $databaseInfo): mysqli
+    private function createInstanceMysqli(DBInfo $DBInfo): mysqli
     {
         return new mysqli(
-            $databaseInfo->DbHost,
-            $databaseInfo->DbUser,
-            $databaseInfo->DbPass,
-            $databaseInfo->DbName,
-            $databaseInfo->DbPort
+            $DBInfo->DbHost,
+            $DBInfo->DbUser,
+            $DBInfo->DbPass,
+            $DBInfo->DbName,
+            $DBInfo->DbPort
         );
     }
 
-    private function creatInstanceMongoDb(DBInfo $databaseInfo): \MongoDB\Database
+    private function creatInstanceMongoDb(DBInfo $DBInfo): \MongoDB\Database
     {
         $client = new \MongoDB\Client(
-            "mongodb://" . $databaseInfo->DbPass . $databaseInfo->DbHost . ":" . $databaseInfo->DbPort ?? "27017"
+            "mongodb://" . $DBInfo->DbPass . $DBInfo->DbHost . ":" . $DBInfo->DbPort ?? "27017"
         );
-        return $client->{$databaseInfo->DbName};
+        return $client->{$DBInfo->DbName};
     }
 
     /**
      * @throws DatabaseInstanceDoesNotExistException
      */
-    public function getInstance(DBInfo $databaseInfo): mysqli|PDO|\MongoDB\Database
+    public function getInstance(DBInfo $DBInfo): mysqli|PDO|\MongoDB\Database
     {
-        if (isset(self::$databases[$databaseInfo->DbName . $databaseInfo->DbType])) {
-            return self::$databases[$databaseInfo->DbName . $databaseInfo->DbType];
+        if (isset(self::$databases[$DBInfo->DbName . $DBInfo->DbType])) {
+            return self::$databases[$DBInfo->DbName . $DBInfo->DbType];
         } else {
             throw new DatabaseInstanceDoesNotExistException();
         }
