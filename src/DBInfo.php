5adaa8350370c33fbd12fd0d17bbc7b1d87108be
alireza ketabi <alirezaketabi.h@gmail.com>
2022-03-07 11:13:49 +0330

    some changes in DBInfo

--- src/DBInfo.php
+++ src/DBInfo.php
@@ -4,12 +4,10 @@ namespace Alireza\DBManagement;
 
 class DBInfo
 {
-    public function __construct(
-        public string $DbType,
-        public string $DbName,
-        public string $DbHost,
-        public string $DbUser,
-        public string $DbPass,
-        public ?int $DbPort = null
-    ){}
+    public string $DbType;
+    public string $DbName;
+    public string $DbHost;
+    public string $DbUser;
+    public string $DbPass;
+    public ?int $DbPort = null;
 }
